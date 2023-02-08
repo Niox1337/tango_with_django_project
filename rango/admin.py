d8fe10e1abb23501bc8e7801962ea5ab329a4abc
Zhixiang Feng <2616718f@student.gla.ac.uk>
2023-02-08 03:28:22 +0000

    TwD6_with_bug

--- rango/admin.py
+++ rango/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.contrib import admin
 from rango.models import Category, Page
 
-# Register your models here.
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ("name",)}
+
+
+admin.site.register(Category, CategoryAdmin)
 
 
 class PageAdmin(admin.ModelAdmin):
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -24,6 +24,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
+    path('category/<slug:category_name_slug>/',
+         views.show_category, name='show_category'),
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
